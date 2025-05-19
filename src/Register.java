463d199e8401d16374f5451601054dd93730c267
Alice <alicewolffernandes@gmail.com>
2025-05-19 14:29:00 -0300

    Little changes done.

---
+++ src/Register.java
@@ -0,0 +1,17 @@
+/*
+Faça um programa para ler a idade, o sexo e a altura de várias pessoas. 
+Calcule e imprima a idade média, o total de pessoas do sexo feminino com 
+idade entre 30 e 45 (inclusive) e a média das alturas dos homens. O programa 
+finaliza quando digitar um valor negativo para idade. 
+ */
+
+import java.util.Scanner;
+
+public class Register {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    
+
+  }
+}
