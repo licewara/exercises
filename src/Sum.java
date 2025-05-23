6452ac72c78a08d8d947b692eb42181daf62d47a
Alice <alicewolffernandes@gmail.com>
2025-05-22 23:56:41 -0300

    New exercise

---
+++ src/Sum.java
@@ -0,0 +1,21 @@
+/*
+Faça um programa que receba um número N indicando a quantidade de valores 
+inteiros a serem lidos.
+
+Em seguida, leia e armazene esses N valores em um vetor.
+
+Calcule a soma de todos os elementos armazenados no vetor e imprima esse valor.
+ */
+
+import java.util.Scanner;
+
+public class Sum {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    
+
+    scanner.close();
+
+  }
+}
