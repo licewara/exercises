a56c098bd61047ee732a6032a93c343558a6012f
Alice <alicewolffernandes@gmail.com>
2025-05-23 01:11:57 -0300

    Fixed SecondDiagonal lets gooooogit status!

--- src/SecondDiagonal.java
+++ src/SecondDiagonal.java
@@ -4,35 +4,29 @@ public class SecondDiagonal {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    int[][] matriz = new int[5][5];
+    int N = 5;
+    int [][] matriz = new int[N][N];
 
-    for (int i = 0; i < 5; i++){
-      for (int j = 0; j < 5; j++){
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++){
 
         matriz[i][j] = scanner.nextInt();
 
       }
     }
 
-    int j = 0;
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++){
+        if (i + j < N - 1){
 
-    for (j = 0; j < 3; j++){
-      System.out.print(matriz[0][j] + " ");
-    }
-    System.out.println(matriz[0][j]);
+          System.out.print(matriz[i][j] + " ");
 
-    for (j = 0; j < 2; j++){
-      System.out.print(matriz[1][j] + " ");
+        }
+      }
+      System.out.println();
     }
-    System.out.println(matriz[1][j]);
-    
-    for (j = 0; j < 1; j++){
-      System.out.print(matriz[2][j] + " ");
-    }
-    System.out.println(matriz[2][j]);
-
-    System.out.print(matriz[3][0]);
 
     scanner.close();
+    
   }
 }
