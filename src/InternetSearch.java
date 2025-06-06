b499e67e524b670481b05daf76673c9cfe670b7c
Raiden <alicewolffernandes@gmail.com>
2025-06-05 23:01:20 -0300

    Solved

---
+++ src/InternetSearch.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InternetSearch {
+  public static void main(String[] args) throws IOException {
+
+      Scanner scanner = new Scanner(System.in);
+      
+      int t = scanner.nextInt();
+      
+      int firstLink = (t * 2) * 2;
+      
+      System.out.println(firstLink);
+
+      scanner.close();
+    }
+ 
+}
