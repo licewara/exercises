2d917624f09ccee1f387fdba225657eb45e96283
Raiden <alicewolffernandes@gmail.com>
2025-06-09 23:02:34 -0300

    Avg2 solved

---
+++ src/Average2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Average2 {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+ 
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        
+        double avg = ((a * 2) + (b * 3) + (c * 5)) / 10;
+        
+        System.out.printf("MEDIA = %.1f\n", avg);
+        
+        scanner.close();
+    }
+ 
+}
