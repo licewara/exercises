d98aa87aca13a2b800a71e64efd9ee1af8206a8a
Raiden <alicewolffernandes@gmail.com>
2025-06-09 23:18:38 -0300

    Solved

---
+++ src/Beginner/Salary.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Salary {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int number = scanner.nextInt();    
+        int hoursWorked = scanner.nextInt();
+        float amountReceived = scanner.nextFloat();
+        
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = U$ %.2f\n", (hoursWorked * amountReceived));
+        
+        scanner.close();
+    }
+ 
+}
