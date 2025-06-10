2f9d2234eef2d2aea1c1a3d11002cc630ddefe57
Raiden <alicewolffernandes@gmail.com>
2025-06-09 22:59:15 -0300

    Solved more problems

---
+++ src/Prod.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prod {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int prod = a * b;
+        
+        System.out.println("PROD = " + prod);
+        
+        scanner.close();
+ 
+    }
+ 
+}
