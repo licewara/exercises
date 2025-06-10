fc9bcd9786f677f01d0214cd3ca4590db1f4ad8b
Raiden <alicewolffernandes@gmail.com>
2025-06-09 23:09:51 -0300

    Solved

---
+++ src/Beginner/Difference.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Difference {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        
+        int diff = (a * b) - (c * d);
+        
+        System.out.println("DIFERENCA = " + diff);
+        
+        scanner.close();
+    }
+ 
+}
