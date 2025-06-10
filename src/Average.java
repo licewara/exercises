7cde495408837456f163be2edc6f5062ea3f00b9
Raiden <alicewolffernandes@gmail.com>
2025-06-09 22:59:15 -0300

    Solved more problems

---
+++ src/Average.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Average {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+        
+        float avg = ((a * 3.5f) + (b * 7.5f)) / 11;
+        
+        System.out.printf("MEDIA = %.5f\n", avg);
+        
+        scanner.close();
+    }
+ 
+}
