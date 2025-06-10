605c712d3f1ba464a662a5a9259d9ca12632c447
Raiden <alicewolffernandes@gmail.com>
2025-06-09 21:32:41 -0300

    Solved basic exercises + Taxes problems

---
+++ src/Basics.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Basics {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        System.out.println("x = " + (a + b));
+
+
+        scanner.close();
+    }
+ 
+}
