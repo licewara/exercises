38f92aee54408632a17c45af692c43d0de21eeb2
Raiden <alicewolffernandes@gmail.com>
2025-06-21 02:06:32 -0300

    Level 3 solved

---
+++ src/Beginner/Snack.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Snack {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        double price;
+
+        int foodCode = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        if (foodCode == 1){
+            price = 4;
+            System.out.printf("Total: R$%.2f %n", (price*quantity));
+        } else if (foodCode == 2){
+            price = 4.50;
+            System.out.printf("Total: R$%.2f %n", (price*quantity));
+        } else if (foodCode == 3){
+            price = 5.00;
+            System.out.printf("Total: R$%.2f %n", (price*quantity));
+        } else if (foodCode == 4){
+            price = 2.00;
+            System.out.printf("Total: R$%.2f %n", (price*quantity));
+        } else if (foodCode == 5){
+            price = 1.50;
+            System.out.printf("Total: R$%.2f %n", (price*quantity));
+        }
+
+        sc.close();
+
+
+    }
+
+}
