bee176530f9b8bcac396e1ceb3d5140b0f276e35
Raiden <alicewolffernandes@gmail.com>
2025-06-23 01:21:17 -0300

    Level 3

--- src/Beginner/Snack.java
+++ src/Beginner/Snack.java
@@ -14,19 +14,19 @@ public class Snack {
 
         if (foodCode == 1){
             price = 4;
-            System.out.printf("Total: R$%.2f %n", (price*quantity));
+            System.out.printf("Total: R$ %.2f%n", (price*quantity));
         } else if (foodCode == 2){
             price = 4.50;
-            System.out.printf("Total: R$%.2f %n", (price*quantity));
+            System.out.printf("Total: R$ %.2f%n", (price*quantity));
         } else if (foodCode == 3){
             price = 5.00;
-            System.out.printf("Total: R$%.2f %n", (price*quantity));
+            System.out.printf("Total: R$ %.2f%n", (price*quantity));
         } else if (foodCode == 4){
             price = 2.00;
-            System.out.printf("Total: R$%.2f %n", (price*quantity));
+            System.out.printf("Total: R$ %.2f%n", (price*quantity));
         } else if (foodCode == 5){
             price = 1.50;
-            System.out.printf("Total: R$%.2f %n", (price*quantity));
+            System.out.printf("Total: R$ %.2f%n", (price*quantity));
         }
 
         sc.close();
